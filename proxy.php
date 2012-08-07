0ea7454c84c15c04e16fe250498160caff9eaebd
Yatao Li <glocklee@gmail.com>
2012-08-08 02:35:42 +0800

    ...

--- proxy.php
+++ proxy.php
@@ -3,7 +3,31 @@ if($_POST['url'] != "")
 	echo file_get_contents( $_POST['url'] );
 else if ( $_GET['nextDiaryOf'] != "" )
 {
-	$page = file_get_contents("http://v-yadli.github.com/diary/diary.html");
-	echo "2012-02-14";
+	$target = $_GET['nextDiaryOf'];
+	require('simple_html_dom.php');
+	$table = array();
+	$next = "";
+	$html = file_get_html('http://v-yadli.github.com/diary/diary.html');
+	$found = FALSE;
+	$acquired = FALSE;
+	foreach($html->find('tr') as $row) {
+		foreach($row->find('td') as $element)
+		{
+			$text = $element->plaintext;
+			if($text == $target)
+			{
+				$found = TRUE;
+			}
+			$next = $text;
+			if($found == TRUE)
+			{
+				$acquired = TRUE;
+				break;
+			}
+		}
+		if($acquired)
+			break;
+	}
+	echo $text;
 }
 ?>
