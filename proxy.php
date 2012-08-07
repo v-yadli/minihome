656695e5b45029bff793e189919ef3ec08cd4f43
Yatao Li <glocklee@gmail.com>
2012-08-08 02:18:18 +0800

    ...

--- proxy.php
+++ proxy.php
@@ -3,6 +3,7 @@ if($_POST['url'] != "")
 	echo file_get_contents( $_POST['url'] );
 else if ( $_GET['nextDiaryOf'] != "" )
 {
+	$page = file_get_contents("http://v-yadli.github.com/diary/diary.html");
 	echo "2012-02-14";
 }
 ?>
