04b988e2711a5c85777fbaf87d661b413c63bde3
Yatao Li <glocklee@gmail.com>
2012-08-08 02:51:32 +0800

    ...

--- proxy.php
+++ proxy.php
@@ -9,12 +9,10 @@ else if ( $_GET['nextDiaryOf'] != "" )
 	$html = file_get_html('http://v-yadli.github.com/diary/diary.html');
 	$found = FALSE;
 	$acquired = FALSE;
-	echo $target;
 	foreach($html->find('tr') as $row) {
 		foreach($row->find('td') as $element)
 		{
 			$next = trim($element->plaintext);
-			echo $next;
 			if($found == TRUE)
 			{
 				$acquired = TRUE;
