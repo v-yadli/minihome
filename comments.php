b879e08ec2052fa1b1fd39b80040c2e8c0b637c0
Yatao Li <glocklee@gmail.com>
2012-08-24 02:16:48 +0800

    ...

--- comments.php
+++ comments.php
@@ -1,4 +1,23 @@
 <?php
+function show_comment()
+{
+    $result = file_get_contents('comments/'+$page_name);
+    if($result == false)
+        echo "No comments yet.";
+    else echo $result;
+}
+
+function add_comment($author,$email,$content)
+{
+    $file_content = file_get_contents( 'comments/'+$page_name );
+    if($file_content == false)
+        $file_content = '';
+    $file_content += "From $author ($email) ".date(DATE_RFC822).":\n".
+                     $content."\n";
+    file_put_contents('comments/'+$page_name, $file_content);
+    echo "Add comment successful.";
+}
+
 $page_name = $_GET['page'] + "_comments.txt";
 echo $page_name;
 if
@@ -22,23 +41,5 @@ if
     }
 }
 
-function show_comment()
-{
-    $result = file_get_contents('comments/'+$page_name);
-    if($result == false)
-        echo "No comments yet.";
-    else echo $result;
-}
-
-function add_comment($author,$email,$content)
-{
-    $file_content = file_get_contents( 'comments/'+$page_name );
-    if($file_content == false)
-        $file_content = '';
-    $file_content += "From $author ($email) ".date(DATE_RFC822).":\n".
-                     $content."\n";
-    file_put_contents('comments/'+$page_name, $file_content);
-    echo "Add comment successful.";
-}
 
 ?>
